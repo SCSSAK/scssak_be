aba9abd04790317ffe25d9d96b54ee3a6b35f344
MessiOfCoding <bty41673662@gmail.com>
2024-11-21 11:22:29 +0900

    fix : mailContent limit 600Bytes

--- src/main/java/com/example/scsa_community2/entity/Mail.java
+++ src/main/java/com/example/scsa_community2/entity/Mail.java
@@ -23,7 +23,7 @@ public class Mail {
     @ManyToOne
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
-
+    @Column(length = 600)
     private String mailContent;
     private LocalDateTime mailCreatedAt;
 }
